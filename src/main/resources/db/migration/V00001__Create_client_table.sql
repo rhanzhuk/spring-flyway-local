3eb30787b5510f57bc0b8e44fcadc971138628e4
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-06-08 14:37:10 +0300

    add new data

--- src/main/resources/db/migration/V00001__Create_client_table.sql
+++ src/main/resources/db/migration/V00001__Create_client_table.sql
@@ -3,4 +3,3 @@ CREATE TABLE client (id INT, name VARCHAR(30),age INT, number VARCHAR(30));
 INSERT INTO client VALUES (1, 'Alfa', 18, '444-444-444');
 INSERT INTO client VALUES (2, 'Beta',30, '333-333-333');
 INSERT INTO client VALUES (3, 'Gamma',21, '937-99-92');
-INSERT INTO client VALUES (4, 'FAILD',00, '00-00-00');
---
+++ src/main/resources/db/migration/V00002__Add_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO client VALUES (4, 'FAILD',00, '00-00-00');
