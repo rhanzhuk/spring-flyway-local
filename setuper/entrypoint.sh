8d0800645aee4e6906a386c4c15b2febeedb5708
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-09-19 18:52:51 +0300

    check token

--- setuper/entrypoint.sh
+++ setuper/entrypoint.sh
@@ -20,5 +20,6 @@ EOF
 
 /app/vault agent -config /app/vault-agent.hcl
 VAULT_TOKEN=$(/app/vault unwrap -field=token $(jq -r '.token' /app/vault-token-via-agent))
-rm -f /app/vault-token-via-agent
-exec /app/envconsul --no-prefix=true -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/myapp/application.properties $@
+cat /app/vault-token-via-agent
+#rm -f /app/vault-token-via-agent
+#exec /app/envconsul --no-prefix=true -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/myapp/application.properties $@
