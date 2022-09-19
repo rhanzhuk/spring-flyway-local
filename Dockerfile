14fa8b14f96ecec133af8812733acdfac05f4c97
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-09-19 17:42:26 +0300

    update all

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
-FROM openjdk:11
+FROM hanzhukruslan/vault-helper:local as vault-helper
+#FROM openjdk:11
+FROM openjdk:16-jdk-alpine
+# TEST jq and later install from vault-helper
+RUN apk add --no-cache jq
 WORKDIR /app
+COPY --from=vault-helper /app/vault /app/vault
+COPY --from=vault-helper /app/envconsul /app/envconsul
+COPY ./setuper/ /app/setuper/
 COPY target/spring_flyway_local-0.0.1-SNAPSHOT.jar /app/spring_flyway_local-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java", "-jar", "spring_flyway_local-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["/app/setuper/entrypoint.sh"]
+CMD ["/bin/sh","-c","env"]
+#CMD ["java", "-jar", "spring_flyway_local-0.0.1-SNAPSHOT.jar"]
---
+++ setuper/entrypoint.sh
@@ -0,0 +1,23 @@
+cat << EOF > /app/vault-agent.hcl
+exit_after_auth = true
+pid_file = "./pidfile"
+auto_auth {
+   method "kubernetes" {
+       mount_path = "auth/kubernetes"
+       config = {
+           role = "read-secret-policy"
+       }
+   }
+   sink "file" {
+       wrap_ttl = "5m"
+       config = {
+           path = "/app/vault-token-via-agent"
+       }
+   }
+}
+EOF
+
+/app/vault agent -config /app/vault-agent.hcl
+VAULT_TOKEN=$(/app/vault unwrap -field=token $(jq -r '.token' /app/vault-token-via-agent))
+rm -f /app/vault-token-via-agent
+exec /app/envconsul --no-prefix=true -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/spring/application.properties $@
