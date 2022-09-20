bcd59d27d09e928ab27c7c085901ab5f471f3e25
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-09-20 12:25:02 +0300

    debugging docker

--- setuper/entrypoint.sh
+++ setuper/entrypoint.sh
@@ -25,3 +25,4 @@ echo $VAULT_ADDR
 rm -f /app/vault-token-via-agent
 #./app/envconsul --no-prefix=true -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/myapp/application.properties
 #env
+exec "$@"
