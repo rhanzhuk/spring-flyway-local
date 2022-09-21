eebb19ddc4b2bafcbe63f3d0c7b800c2a85cebe6
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-09-21 12:57:17 +0300

    ALARM set role

--- setuper/entrypoint.sh
+++ setuper/entrypoint.sh
@@ -6,7 +6,7 @@ auto_auth {
    method "kubernetes" {
        mount_path = "auth/kubernetes"
        config = {
-           role = "example"
+           role = "test"
        }
    }
    sink "file" {
@@ -21,9 +21,6 @@ EOF
 /app/vault agent -config /app/vault-agent.hcl
 VAULT_TOKEN=$(/app/vault unwrap -field=token $(jq -r '.token' /app/vault-token-via-agent))
 echo $VAULT_TOKEN
-echo $VAULT_ADDR
 rm -f /app/vault-token-via-agent
-#/app/envconsul --no-prefix=true -upcase -sanitize -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/myapp/application.properties env
 exec /app/envconsul --no-prefix=true -upcase -sanitize -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/myapp/application.properties "$@"
-#env
 #exec env "$@"
