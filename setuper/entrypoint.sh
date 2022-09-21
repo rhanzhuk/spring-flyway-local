036311c73e7e183cf3f4c6d4a2469ca6ebfab53b
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-09-21 16:00:41 +0300

    configuration entrypoint

--- setuper/entrypoint.sh
+++ setuper/entrypoint.sh
@@ -10,7 +10,7 @@ auto_auth {
        }
    }
    sink "file" {
-       wrap_ttl = "5m"
+       wrap_ttl = "2m"
        config = {
            path = "/app/vault-token-via-agent"
        }
@@ -20,7 +20,5 @@ EOF
 
 /app/vault agent -config /app/vault-agent.hcl
 VAULT_TOKEN=$(/app/vault unwrap -field=token $(jq -r '.token' /app/vault-token-via-agent))
-echo $VAULT_TOKEN
 rm -f /app/vault-token-via-agent
-exec /app/envconsul --no-prefix=true -upcase -sanitize -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/myapp/application.properties "$@"
-#exec env "$@"
+exec /app/envconsul --no-prefix=true -upcase -sanitize -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/app-conf/spring-flyway-local/application.properties "$@"
