d7548ba6daa681783edf0976931eb34e2ac88ecb
rhanzhuk <hanzhuk_ruslan@icloud.com>
2022-09-22 10:52:18 +0300

    generate base image

--- setuper/entrypoint.sh
+++ setuper/entrypoint.sh
@@ -18,7 +18,7 @@ auto_auth {
 }
 EOF
 
-/app/vault agent -config /app/vault-agent.hcl
-VAULT_TOKEN=$(/app/vault unwrap -field=token $(jq -r '.token' /app/vault-token-via-agent))
+/bin/vault agent -config /app/vault-agent.hcl
+VAULT_TOKEN=$(/bin/vault unwrap -field=token $(jq -r '.token' /app/vault-token-via-agent))
 rm -f /app/vault-token-via-agent
-exec /app/envconsul --no-prefix=true -upcase -sanitize -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/app-conf/spring-flyway-local/application.properties "$@"
+exec /bin/envconsul --no-prefix=true -upcase -sanitize -once -vault-renew-token=false -vault-addr=${VAULT_ADDR} -vault-token=${VAULT_TOKEN} -secret secret/data/app-conf/spring-flyway-local/application.properties "$@"
